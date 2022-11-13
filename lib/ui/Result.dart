ad7285d8f16dbed9c224250edf8eb2a308a0de23
shiny8859 <shiny8859@gmail.com>
2022-11-13 15:27:50 +0900

    リザルト画面の作成

--- lib/ui/Result.dart
+++ lib/ui/Result.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Result extends StatefulWidget {
   @override
@@ -8,15 +9,47 @@ class Result extends StatefulWidget {
 class _Result extends State<Result> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Button'),
-          onPressed: () {
-            Navigator.pushNamed(context, "/");
-          },
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme:
+              GoogleFonts.reggaeOneTextTheme(Theme.of(context).textTheme),
         ),
-      ),
-    );
+        home: Scaffold(
+          backgroundColor: Colors.brown[500],
+          appBar: AppBar(
+            title: Text("リザルト"),
+            centerTitle: true,
+            backgroundColor: Colors.brown[400],
+          ),
+          body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text("おめでとう！\n1/20問正解！\nAランク 0体\nBランク 0体\nCランク 1体\n倒しました★",
+                      style: TextStyle(fontSize: 36)),
+                  ElevatedButton(
+                    child: const Text('問題の解説'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.brown[800],
+                      fixedSize: Size(200, 100),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/");
+                    },
+                  ),
+                  ElevatedButton(
+                    child: const Text('トップへ'),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        fixedSize: Size(200, 100),
+                        onPrimary: Colors.black),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/");
+                    },
+                  ),
+                ]),
+          ),
+        ));
   }
 }
