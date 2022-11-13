856659fb720dd017814ee305f879fc7e20454726
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-11-13 09:23:00 +0900

    firebaseの設定完了

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'package:en_joint/ui/Home.dart';
 import 'package:en_joint/ui/Result.dart';
 import 'package:en_joint/ui/Room.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
